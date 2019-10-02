02010b396742b3c14f82d1c6741cc91f0a161136
Jasper De Moor <jasperdemoor@gmail.com>
2019-10-02 13:50:54 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import yarnInstall from "./yarn/install";
 async function start() {
   let parcelTwoDir = path.join(process.cwd(), ".tmp/parcel-v2");
 
-  await gitClone("git@github.com:parcel-bundler/parcel.git", parcelTwoDir);
+  await gitClone("https://github.com/parcel-bundler/parcel.git", parcelTwoDir);
   await gitCheckout(parcelTwoDir, "v2");
   await yarnInstall(parcelTwoDir);
 }
