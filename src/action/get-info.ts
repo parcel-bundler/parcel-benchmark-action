3fe53d53f4d3559e3073f1cd3771f702061851df
Jasper De Moor <jasperdemoor@gmail.com>
2019-10-02 14:19:41 +0200

    setup cloning

---
+++ src/action/get-info.ts
@@ -0,0 +1,42 @@
+export default function getInfo() {
+  const {
+    ISSUE_ID,
+    SKIP_CLONE,
+    GITHUB_REF,
+    GIT_ROOT_DIR,
+    GITHUB_ACTION,
+    GITHUB_REPOSITORY,
+    GITHUB_EVENT_PATH,
+    PR_STATS_COMMENT_TOKEN
+  } = process.env;
+
+  if (process.env.GITHUB_TOKEN) {
+    delete process.env.GITHUB_TOKEN;
+    delete process.env.PR_STATS_COMMENT_TOKEN;
+  }
+
+  let info = {
+    skipClone: SKIP_CLONE || false,
+    actionName: GITHUB_ACTION || "opened",
+    githubToken: PR_STATS_COMMENT_TOKEN,
+    gitRoot: GIT_ROOT_DIR || "https://github.com/",
+    prRepo: GITHUB_REPOSITORY || "https://github.com/parcel-bundler/parcel.git",
+    prRef: GITHUB_REF || "console-patch-fix",
+    issueId: ISSUE_ID || "1"
+  };
+
+  // get comment
+  if (GITHUB_EVENT_PATH) {
+    let event = require(GITHUB_EVENT_PATH);
+    info.actionName = event.action || info.actionName;
+
+    let prData = event["pull_request"];
+    if (prData) {
+      info.prRepo = prData.head.repo.full_name;
+      info.prRef = prData.head.ref;
+      info.issueId = prData.number;
+    }
+  }
+
+  return info;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import path from "path";
 
+import getActionInfo from "./action/get-info";
+
 import gitClone from "./git/clone";
 import gitCheckout from "./git/checkout";
 import yarnInstall from "./yarn/install";
 
-async function start() {
-  let parcelTwoDir = path.join(process.cwd(), ".tmp/parcel-v2");
+const ALLOWED_ACTIONS = new Set(["synchronize", "opened"]);
 
-  await gitClone("https://github.com/parcel-bundler/parcel.git", parcelTwoDir);
-  await gitCheckout(parcelTwoDir, "v2");
+const BASE_REPO = "https://github.com/parcel-bundler/parcel.git";
+const BASE_BRANCH = "v2";
+
+async function start() {
+  let actionInfo = getActionInfo();
+
+  // Skip everything...
+  if (actionInfo.skipClone) return;
+
+  let parcelTwoDir = path.join(process.cwd(), ".tmp/parcel-v2");
+  console.log("Cloning Parcel Repository...");
+  await gitClone(BASE_REPO, parcelTwoDir);
+  await gitCheckout(parcelTwoDir, BASE_BRANCH);
   await yarnInstall(parcelTwoDir);
+
+  let prDir = path.join(process.cwd(), ".tmp/parcel-pr");
+  console.log("Cloning PR Repository...");
+  await gitClone(actionInfo.prRepo, prDir);
+  await gitCheckout(prDir, actionInfo.prRef);
+  await yarnInstall(prDir);
 }
 
 start();
