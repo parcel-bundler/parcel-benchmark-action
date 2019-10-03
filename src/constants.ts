c20fb0ea37262ef27ed7a9e9d2bd20a5c1d70a71
Jasper De Moor <jasperdemoor@gmail.com>
2019-10-03 19:52:42 +0200

    use public api

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const PARCEL_EXAMPLES = ["packages/benchmarks/kitchen-sink"];
 export const REPO_OWNER = "DeMoorJasper";
 export const REPO_NAME = "parcel-1";
 export const REPO_BRANCH = "v2";
-export const API_URL = "http://localhost:3000/api/";
+export const API_URL = "https://parcel-benchmark.blazingly.io/api/";
