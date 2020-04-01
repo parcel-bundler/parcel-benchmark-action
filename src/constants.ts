1b277a0edc6ba0671eecc1512cd0088db12819da
Jasper De Moor <jasperdemoor@gmail.com>
2020-04-01 19:47:52 +0200

    revert to 3 runs

--- src/constants.ts
+++ src/constants.ts
@@ -24,4 +24,4 @@ export const REPO_OWNER = 'parcel-bundler';
 export const REPO_NAME = 'parcel';
 export const REPO_BRANCH = 'v2';
 export const API_URL = 'https://benchmark.parceljs.org/api/';
-export const AMOUNT_OF_RUNS = 1;
+export const AMOUNT_OF_RUNS = 3;
